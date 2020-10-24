05a5220503dc12f635c455387efdf7575cd11cea
Pedro Teixeira <pedro.h.d.teixeira@gmail.com>
2020-10-24 23:48:19 +0100

    fix broken server-side JS with react hooks

--- pages/cv.js
+++ pages/cv.js
@@ -1,15 +1,16 @@
 import Head from "next/head";
+import React, { useEffect } from 'react';
 
 export default function CV() {
 
-  // detect mobile device and display CV as button rather than embedded
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  let cvElem;
-  if (isMobile) {
-    cvElem = <Button href="pedrohdt-cv.pdf" style={{backgroundColor: "#e4572e"}}>LinkedIn</Button>;
-  } else {
-    cvElem = <object data="pedrohdt-cv.pdf" type="application/pdf" width="100%" height="100%" />;
-  }
+  let cvElem = <object data="pedrohdt-cv.pdf" type="application/pdf" width="100%" height="100%" />;
+
+  useEffect(() => {
+    // detect mobile device and display CV as button rather than embeddned
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      cvElem = <Button href="pedrohdt-cv.pdf" style={{backgroundColor: "#e4572e"}}>LinkedIn</Button>;
+    }
+  });
 
   return (
     <>
