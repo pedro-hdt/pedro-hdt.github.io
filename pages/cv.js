24448f4847b95a0de01a0a7ce049f130854d5e72
Pedro Teixeira <pedro.h.d.teixeira@gmail.com>
2020-10-24 23:34:13 +0100

    change CV on mobile to button

--- pages/cv.js
+++ pages/cv.js
@@ -1,6 +1,16 @@
 import Head from "next/head";
 
 export default function CV() {
+
+  // detect mobile device and display CV as button rather than embedded
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  let cvElem;
+  if (isMobile) {
+    cvElem = <Button href="pedrohdt-cv.pdf" style={{backgroundColor: "#e4572e"}}>LinkedIn</Button>;
+  } else {
+    cvElem = <object data="pedrohdt-cv.pdf" type="application/pdf" width="100%" height="100%" />;
+  }
+
   return (
     <>
       <Head>
@@ -8,7 +18,7 @@ export default function CV() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <object data="pedrohdt-cv.pdf" type="application/pdf" width="100%" height="100%" />
+      {cvElem}      
         
     </>
   );
